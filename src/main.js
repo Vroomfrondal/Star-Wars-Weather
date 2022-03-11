1f67c49b05737893c82d88fb919ef1bf7530c18a
Christopher DeLeon <lionscantcode@gmail.com>
2022-03-11 00:39:10 -0600

    render API data
    - added pictures
    
    - checkLocationPermission() of user > getWeather() from API > displayWeather() > run planets algorithm
    
    - minor styles/css / index.html changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-getLocation = () => {
+checkLocationPermission = () => {
     // get coordinates of user for use in Open Weather API
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -9,22 +9,42 @@ getLocation = () => {
     }
 }
 
-getWeather = async (latitude, longitude) => {
-    // fetch OpenWeather API based on user's coordinates
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=6db2a3f65fec5bc47108235e85affbc9&units=imperial`)
-
+//Used async/await because fetch returns a promise and I don't have to use .then :))
+const getWeather = async (latitude, longitude) => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${config.OPEN_WEATHER_API_KEY}&units=imperial`)
     // ensure API is up and running
     if (response.status === 200) {
         const weatherData = await response.json()
-
-        // console.log(`Weather for ${weatherData.name}:`)
-        // console.log(weatherData)
         render(weatherData)
     } else {
+        console.log("error")
         throw new Error("Sorry, OpenWeather seems to be having issues with their API. Try again later.")
     }
 }
 
-const render = (weatherData) => {}
+// Planet Algorithm will take into account temperature and forecast conditions and determine which planet to display
+determinePlanet = (filteredTemp) => {
+    let planet
 
-getLocation()
+    if (filteredTemp > 60) {
+        console.log("Display Planet")
+        planet = "Hi"
+    }
+
+    return planet
+}
+
+const render = (weatherData) => {
+    const tempElement = document.querySelector("#temp")
+    const descElement = document.querySelector("#description")
+    const planetElement = document.querySelector("#planet")
+    const filteredTemp = weatherData.main.temp.toFixed()
+
+    // populate DOM
+    tempElement.textContent = filteredTemp
+    descElement.textContent = weatherData.weather[0].description
+    planetElement.textContent = determinePlanet(filteredTemp)
+
+    console.log(weatherData) //debug
+}
+checkLocationPermission()
